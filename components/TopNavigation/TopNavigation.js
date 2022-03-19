9cef0ecf7f7263dcda62add3268e7ff5c309f6a3
omittee <88842886+omittee@users.noreply.github.com>
2022-03-19 11:08:52 +0800

    update components

---
+++ components/TopNavigation/TopNavigation.js
@@ -0,0 +1,23 @@
+// components/TopNavigation/TopNavigation.js
+const app=getApp()
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        topText:Number
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        statusBarHeight: wx.getSystemInfoSync()['statusBarHeight'],
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+    }
+})
